f71c91f297090ca41f1615446218f6f7ea152cf4
Luka Seamus Wright <luka@wright.it>
2026-04-24 11:02:33 +0800

    Calculated CSP

--- CSP/CSP.R
+++ CSP/CSP.R
@@ -1,39 +1,122 @@
 # 1. Load data ####
 require(tidyverse)
 require(here)
-grazing_prior_posterior <- 
-  read_rds(here("Urchins", "RDS", "grazing_prior_posterior.rds"))
-C_diff <- 
-  read_rds(here("Biochemistry", "C_N", "RDS", "C_diff.rds"))
-distance_diff <- 
-  read_rds(here("Sinking", "RDS", "distance_diff.rds"))
-
-# 2. Calculation ####
 require(magrittr)
-CSP <- grazing_prior_posterior %>%
-  filter(Season == "Annual") %>%
-  mutate(Defecation_prop = beta / 100) %>% # Convert to proportion
-  select(starts_with("."), Defecation_prop) %>%
-  full_join(
-    C_diff %>%
-      filter(parameter == "obs_new") %>%
-      select(starts_with("."), prop) %>%
-      rename(Carbon_prop = prop),
-    by = c(".chain", ".iteration", ".draw")
-  ) %>%
-  full_join(
-    distance_diff %>%
-      filter(parameter == "mu") %>%
-      select(starts_with("."), prop) %>%
-      rename(Distance_prop = prop),
-    by = c(".chain", ".iteration", ".draw")
-  ) %>%
-  mutate(CSP = Defecation_prop * Carbon_prop * Distance_prop) %T>%
+# There is only enough data on Strongylocentrotus droebachiensis to calculate
+# carbon sequestration potential (CSP). I am expressing CSP of faeces relative
+# to the kelp baseline.
+
+# Defecated fraction
+defecation <- here("Urchins", "RDS", "meta_prior_posterior_s.rds") %>%
+  read_rds() %>%
+  filter(Species == "Strongylocentrotus droebachiensis") %>%
+  droplevels() %T>%
   print()
 
-CSP %>%
-  summarise(mean = mean(CSP),
-            sd = sd(CSP),
-            n = n())
+# Carbon fraction
+carbon <- here("Biochemistry", "RDS", "biochem_prior_posterior_s.rds") %>%
+  read_rds() %>%
+  filter(Compound == "Carbon" & Species == "Strongylocentrotus droebachiensis") %>%
+  droplevels() %T>%
+  print()
 
-# 3. Visualisation ####
+# Depth
+depth <- here("Sinking", "RDS", "depth_diff.rds") %>%
+  read_rds() %>%
+  filter(parameter == "obs") %T>%
+  print()
+
+# 2. Calculation ####
+# Combine data
+CSP <- defecation %>%
+  select(starts_with("."), Proportion) %>%
+  rename(Mass = Proportion) %>%
+  full_join(
+    carbon %>%
+      select(starts_with("."), Ratio) %>%
+      rename(Carbon = Ratio)
+  ) %>%
+  full_join(
+    depth %>%
+      mutate(Ratio = 10^logratio) %>%
+      select(starts_with("."), Ratio) %>%
+      rename(Export = Ratio)
+  ) %T>%
+  print()
+
+# Calculate carbon transfer through urchin
+CSP %<>%
+  mutate(Transfer = Mass * Carbon) %T>%
+  print()
+  
+# Calculate CSP based on carbon transfer and export
+CSP %<>%
+  mutate(CSP = Transfer * Export) %T>%
+  print()
+
+# 3. Summary ####
+require(glue)
+CSP_summary <- CSP %>%
+  pivot_longer(
+    cols = !starts_with("."),
+    names_to = "Measure",
+    values_to = "Ratio" # All measures represent faeces / kelp
+  ) %>%
+  mutate(log_Ratio = log(Ratio)) %>%
+  summarise(
+    across(
+      c(Ratio, log_Ratio),
+      list(
+        mean = mean,
+        sd = sd,
+        median = median
+      )
+    ),
+    P = mean( Ratio < 1 ), # equivalent to log_Ratio < 0
+    n = n(),
+    .by = Measure
+  ) %>%
+  mutate(
+    across(
+      c(contains("mean"), contains("sd"), contains("median"), "P"),
+      ~if_else(.x < 100, signif(.x, 2), signif(.x, 3))
+    ),
+    Ratio = glue( "{Ratio_median} ({log_Ratio_mean} ± {log_Ratio_sd})" )
+  ) %>%
+  select(!(contains("mean") | contains("sd") | contains("median"))) %T>%
+  print()
+
+CSP_summary %>%
+  write_csv(here("CSP", "CSP.csv"))
+
+require(officer)
+read_docx() %>%
+  body_add_table(
+    value = CSP_summary
+  ) %>%
+  print(target = here("CSP", "CSP.docx"))
+
+# 4. Visualisation ####
+CSP %>%
+  ggplot() +
+    geom_vline(xintercept = 1) +
+    geom_density(aes(Transfer)) +
+    scale_x_log10() +
+    theme_minimal()
+
+CSP %>%
+  ggplot() +
+    geom_vline(xintercept = 1) +
+    geom_density(aes(Export)) +
+    scale_x_log10() +
+    theme_minimal()
+
+CSP %>%
+  ggplot() +
+    geom_vline(xintercept = 1) +
+    geom_density(aes(CSP)) +
+    scale_x_log10() +
+    theme_minimal()
+# The putative benefit of sinking speed is mostly cancelled by minimal carbon transfer.
+# If other factors such as microbial decomposition are also considered, it is likely
+# that faecal carbon is completely mineralised before it can be exported.
